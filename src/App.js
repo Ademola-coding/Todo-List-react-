358fe94001de6edc8f2c86a742e547f7e5194238
ademola <ademolaowoeye07@gmail.com>
2022-09-21 19:55:53 +0100

    linters

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
-function App() { 
-  return ( 
-  <div className="App">
-     <h1>Todolist</h1>
-      </div> 
-    );
+function App() {
+  return (
+    <div className="App">
+      <h1>Todolist</h1>
+    </div>
+  );
 }
 
 export default App;
